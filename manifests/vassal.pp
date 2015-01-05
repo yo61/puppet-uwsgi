579ec2ead5bf95a739bd63cc59669f5455f81355
Robin Bowes <robin.bowes@yo61.com>
2015-01-05 14:14:32 +0000

    Add ensure parameter to allow vassals to be removed

--- manifests/vassal.pp
+++ manifests/vassal.pp
@@ -1,21 +1,30 @@
 define uwsgi::vassal(
   $vassal_user,
   $vassal_group,
+  $ensure = 'present',
   $ini_source  = undef,
   $ini_content = undef,
 ) {
-  
+ 
+  validate_re($ensure, '^(present|absent)$',
+    "${ensure} is not supported for ensure. Allowed values are 'present' and 'absent'."
+  )
+
   $both = $ini_source and $ini_content
   $neither = !$ini_source and !$ini_content
-
   if $both or $neither {
     fail('Only one of "source" and "content" parameters should be specified')
   }
 
   include ::uwsgi::params
 
+  $ini_ensure = $ensure ? {
+    present => 'file',
+    default => $ensure,
+  }
+
   file{"${::uwsgi::params::emperor_dir}/${name}.ini":
-    ensure  => file,
+    ensure  => $ini_ensure,
     content => $ini_content,
     source  => $ini_source,
     user    => $vassal_user,
